30b1bdd6f5826974b2e6c504ece9024caa5aaeb1
Emmanuel Bernal <emmanuel.bernal@walmart.com>
2023-04-13 20:48:43 -0500

    INIT | Starting

---
+++ app/src/main/java/com/wizeline/dependencyinjection/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.wizeline.dependencyinjection.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wizeline.dependencyinjection.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
