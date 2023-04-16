baf8132da071e925ec106bf462a0ad428535a0c7
Emmanuel Bernal <emmanuel.bernal@walmart.com>
2023-04-15 19:37:26 -0500

    Logic of AppNavigator implemented without Hilt

--- app/src/main/java/com/wizeline/dependencyinjection/ui/MainActivity.kt
+++ app/src/main/java/com/wizeline/dependencyinjection/ui/MainActivity.kt
@@ -3,10 +3,26 @@ package com.wizeline.dependencyinjection.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.wizeline.dependencyinjection.R
+import com.wizeline.dependencyinjection.navigation.AppNavigator
+import com.wizeline.dependencyinjection.navigation.Screens
 
 class MainActivity : AppCompatActivity() {
+
+
+   lateinit var appNavigator: AppNavigator
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            appNavigator.navigateTo(Screens.ORDER)
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        if (supportFragmentManager.backStackEntryCount == 0) {
+            finish()
+        }
     }
 }
